dc29c46414ce3d2669b09c405240966e180990e4
Dericioso <dery.lantigua@gmail.com>
2022-04-15 12:18:22 +0200

    Modify app file

--- intro-react/src/App.js
+++ intro-react/src/App.js
@@ -33,7 +33,87 @@ const App = () => {
   }
 
   const handleAddTodo = (e) => {
-    e.preventDefault();
+    e.preventDefault();import React, { useState, useRef, useEffect } from 'react';
+    // import './App.css';
+    import { Header, Button, Tasks, CompleteTask } from './components';
+    import {v4 as uuidv4} from 'uuid'
+    
+    
+    function App() {
+      const taskToDo = useRef();
+      const LOCAL_STORAGE_KEY = 'taskApp'
+      const [tasks, setTasks] = useState([]);
+      // const [completeTask, setCompleteTask] = useState([]);
+      const id = uuidv4();
+    
+      useEffect(() => {
+        const storedTask = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if (storedTask) {
+          setTasks(storedTask);
+        }
+      }, [])
+    
+      useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(tasks))
+      }, [tasks])
+    
+      const toggleTask = (id) => {
+        const newTask = [...tasks];
+        const oneTask = newTask.find(oneTask => oneTask.id === id);
+        oneTask.complete = !oneTask.complete;
+        setTasks(newTask);
+      }
+    
+      const handleAdd = (e) => {
+        e.preventDefault();
+        let name = taskToDo.current.value;
+        if (name === '') {
+          return;
+        }
+        setTasks(prevTask => {
+          return [...prevTask, {id: id, name: name, complete: false}]
+        })
+        taskToDo.current.value = null;
+      }
+    
+      const doneTask = () => {
+        const taskDone = tasks.filter(todoList => !todoList.complete);
+        setTasks(taskDone) ;
+      }
+    
+      // const handleDoneTask = () => {
+      //   const displayCompleteTask = [...tasks];
+      //   const oldTask = displayCompleteTask.find(oldTask => oldTask.id === id);
+      //   oldTask.complete = !oldTask.complete;
+      //   setCompleteTask(displayCompleteTask);
+      //   console.log(displayCompleteTask);
+      // }
+    
+      // const deleteTask = (id) => {
+      //   setTasks(tasks.filter((todoList) => todoList.id !== id));
+      // }
+    
+      return (
+        <>
+        <div className="grid grid-cols-3 justify-center mt-5">
+          <div></div>
+          <div className='border-2 border-black text-center'>
+            <Header headerName={"Today's To Do"} />
+            <input className='border-2 border-black' type="text" ref={taskToDo} />
+            <Button btnName={'Add'} onClick={handleAdd} />
+            {tasks.length} <br />
+            <Tasks tasks={tasks} toggleTask={toggleTask} />
+            <Button btnName={'Complete'} onClick={doneTask} />
+            <CompleteTask onChange={''} />
+          </div>
+          <div></div>
+        </div>
+        </>
+      )
+    }
+    
+    export default App;
+    
     let name = todoNameRef.current.value;
     if (name === '') {
       return;
--- intro-react/src/index.js
+++ intro-react/src/index.js
@@ -1,14 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 import * as ReactDOMClient from 'react-dom/client'; 
 import './index.css'; 
 import App from './App';  
