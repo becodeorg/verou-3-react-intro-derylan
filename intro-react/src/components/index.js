10bb612c5eaed635c29c5a11acabb0a79772d76f
Dericioso <dery.lantigua@gmail.com>
2022-04-15 12:23:22 +0200

    Create index file to export the components

---
+++ intro-react/src/components/index.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+import Button from './Button';
+import Tasks from './Tasks';
+import CompleteTask from './CompleteTask';
+
+export {
+    Header,
+    Button,
+    Tasks,
+    CompleteTask
+}
